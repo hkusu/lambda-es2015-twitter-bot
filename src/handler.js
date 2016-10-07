34f90329a74b102447dbee4835b27e1329044c6d
hkusu <kusu0806@gmail.com>
2016-10-07 21:55:20 +0900

    Update libraries and change to use async/await

--- src/handler.js
+++ src/handler.js
@@ -3,8 +3,7 @@ import axios from 'axios'
 import moment from 'moment'
 import Twitter from './Twitter'
 
-// read configuration from .env file
-dotenv.config()
+dotenv.config() // read configuration from .env file
 
 export default (event, context, callback) => {
   let { city } = event // case of via API Gateway
@@ -15,15 +14,19 @@ export default (event, context, callback) => {
   const twitter = new Twitter()
   const currentTimeStr = moment().utc().add(9, 'h').format('《MM月DD日 HH時mm分》') // current time (Japan)
 
+  const run = async () => {
+    const res = await axios.get(`http://weather.livedoor.com/forecast/webservice/json/v1?city=${city}`) // livedoor Weather Hacks
+
+    let message = currentTimeStr + res.data.description.text
+    message = message.replace(/\r?\n/g, '') // remove return code
+    message = message.replace(/ /g, '') // remove space
+    message = message.substr(0, 140) // 140 characters from the head (limited by Twitter)
+
+    await twitter.tweet(message) // tweet
+  }
+
   // tweet weather
-  axios.get(`http://weather.livedoor.com/forecast/webservice/json/v1?city=${city}`) // livedoor Weather Hacks
-    .then(res => {
-      let message = currentTimeStr + res.data.description.text
-      message = message.replace(/\r?\n/g, '') // remove return code
-      message = message.replace(/ /g, '') // remove space
-      return message.substr(0, 140) // 140 characters from the head (limited by Twitter)
-    })
-    .then(message => twitter.tweet(message)) // tweet
+  run()
     .then(() => callback(null, 'success'))
     .catch(err => callback(err))
 }

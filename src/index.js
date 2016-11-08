8ae34015115e721228e8911a66e11887f45f6268
hkusu <kusu0806@gmail.com>
2016-11-08 22:42:39 +0900

    Change variable

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ export function handler(event, context, callback) {
   const twitter = new Twitter()
   const currentTimeStr = moment().utc().add(9, 'h').format('《MM月DD日 HH時mm分》') // current time (Japan)
 
-  const run = async () => {
+  const promise = (async () => {
     const weatherData = await weather.get(city)
 
     let message = currentTimeStr + weatherData.data.description.text
@@ -25,10 +25,10 @@ export function handler(event, context, callback) {
     message = message.substr(0, 140) // 140 characters from the head (limited by Twitter)
 
     await twitter.tweet(message) // tweet
-  }
+  })()
 
   // tweet weather
-  run()
+  promise
     .then(() => callback(null, 'success'))
     .catch(err => callback(err))
 }
